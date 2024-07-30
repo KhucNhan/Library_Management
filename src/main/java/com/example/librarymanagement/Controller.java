f0f4ecc61eff78a59c3416799799e2ed2fb5fd13
Khuc Nhan <khucvii@gmail.com>
2024-07-30 11:44:09 +0700

    update new login feature for admin only

--- src/main/java/com/example/librarymanagement/Controller.java
+++ src/main/java/com/example/librarymanagement/Controller.java
@@ -5,11 +5,16 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Controller {
-    User user1 = new User("1", "nhan", "nhan", "admin");
-    private User[] users = {user1};
+    User user1 = new Admin("Nhan12345","khucnhan","nhan2005", "admin");
+    User user2 = new Admin("Khanh23456","baokhanh","khanh2005", "admin");
+    User user3 = new Admin("Dam34567","vandam","dam2005", "admin");
+    User user4 = new User("Phuong45678","nguyenphuong","phuong2005", "user");
+    public User[] users = {user1, user2, user3, user4};
     @FXML
     TextField username;
     @FXML
@@ -23,33 +28,11 @@ public class Controller {
     public void setRole(String username, String role) {
         findUser(username).setRole(role);
     }
-
-    public User findUser(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return user;
-            }
-        }
-        return null;
-    }
-
-    public boolean findUserBoolean(String username) {
-        for (User user : users) {
-            if (user.getUsername().equals(username)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-
     @FXML
     protected void login() {
         String username = this.username.getText();
         String password = this.password.getText();
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-
-        System.out.println(users.length);
 
         if (!findUserBoolean(username)) {
             alert.setContentText("Username không tồn tại.");
@@ -71,5 +54,21 @@ public class Controller {
         for (User user : users) {
             System.out.println(user.toString());
         }
+    }
+    public User findUser(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+    public boolean findUserBoolean(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
