8cb7113376b19d4ad2e2557d790a62f727a46958
baokhanh <thehongbaokhanh2005@gmail.com>
2024-07-31 13:57:35 +0700

    chuan bi pull

--- src/main/java/com/example/librarymanagement/Main.java
+++ src/main/java/com/example/librarymanagement/Main.java
@@ -18,13 +18,6 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        Controller master = new Controller();
-        master.add(new Admin("Nhan12345","khucnhan","nhan2005", "admin"));
-        master.add(new Admin("Khanh23456","baokhanh","khanh2005", "admin"));
-        master.add(new Admin("Dam34567","vandam","dam2005", "admin"));
-        master.add(new User("Phuong45678","nguyenphuong","phuong2005", "user"));
-//        master.setRole("nguyenphuong", "admin");
-        master.display();
         launch();
     }
 }
