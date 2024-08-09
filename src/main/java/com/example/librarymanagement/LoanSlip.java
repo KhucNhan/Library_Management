de36afaa1905999f67cdb30cf26aed47919f19c8
Khuc Nhan <khucvii@gmail.com>
2024-08-09 10:54:22 +0700

    test

--- src/main/java/com/example/librarymanagement/LoanSlip.java
+++ src/main/java/com/example/librarymanagement/LoanSlip.java
@@ -1,1 +1,64 @@
+package com.example.librarymanagement;
 
+import javafx.scene.Parent;
+
+import java.util.ArrayList;
+
+public class LoanSlip {
+    private String idUser;
+    private String idBook;
+    private String date;
+    private String returnDate;
+    private String status;
+
+    public LoanSlip() {
+    }
+
+    public LoanSlip(String idUser, String idBook, String date, String returnDate, String status) {
+        this.idUser = idUser;
+        this.idBook = idBook;
+        this.date = date;
+        this.returnDate = returnDate;
+        this.status = status;
+    }
+
+    public void setIdUser(String idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getIdBook() {
+        return idBook;
+    }
+
+    public void setIdBook(String idBook) {
+        this.idBook = idBook;
+    }
+
+    public String getIdUser() {
+        return idUser;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(String returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
--- src/main/java/com/example/librarymanagement/LoanSlipController.java
+++ src/main/java/com/example/librarymanagement/LoanSlipController.java
@@ -13,18 +13,31 @@ import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class LoanSlipController {
+public class LoanSlipController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
+
     public void goToLoginScene(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root, 1080, 720);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void goToAddBookScene(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AddBook.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 1080, 720);
         stage.setScene(scene);
@@ -35,153 +48,47 @@ public class LoanSlipController {
         Parent root = FXMLLoader.load(getClass().getResource("ListView.fxml"));
         stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root, 1080, 720);
-        stage.setTitle("Loan Slip");
+        stage.setTitle("Home");
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    private TableView<LoanSlip> tableView;
+    @FXML
+    private TableColumn<LoanSlip, String> idUserCol;
+    @FXML
+    private TableColumn<LoanSlip, String> idBookCol;
+    @FXML
+    private TableColumn<LoanSlip, String> dateCol;
+    @FXML
+    private TableColumn<LoanSlip, String> returnDateCol;
+    @FXML
+    private TableColumn<LoanSlip, String> statusCol;
+
+    private ObservableList<LoanSlip> loanSlips;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        loanSlips = FXCollections.observableArrayList(
+                new LoanSlip("Nhan12345", "b1", "9/8/2024", "10/9/2024", "true"),
+                new LoanSlip("Nhan12345", "b2", "9/8/2024", "10/9/2024", "true"),
+                new LoanSlip("Nhan12345", "b3","9/8/2024", "10/9/2024", "true"),
+                new LoanSlip("Nhan12345", "b4", "9/8/2024", "10/9/2024", "true")
+        );
+        idUserCol.setCellValueFactory(new PropertyValueFactory<LoanSlip, String>("idUser"));
+        idBookCol.setCellValueFactory(new PropertyValueFactory<LoanSlip, String>("idBook"));
+        dateCol.setCellValueFactory(new PropertyValueFactory<LoanSlip, String>("date"));
+        returnDateCol.setCellValueFactory(new PropertyValueFactory<LoanSlip, String>("returnDate"));
+        statusCol.setCellValueFactory(new PropertyValueFactory<LoanSlip, String>("status"));
+        tableView.setItems(loanSlips);
+    }
+
+    @FXML
+    public void ChangeStatus() {
+        LoanSlip selectedLoanSlip = tableView.getSelectionModel().getSelectedItem();
+        if (selectedLoanSlip != null) {
+            selectedLoanSlip.setStatus("false");
+        }
+    }
 }
-//
-//public class LoanSlipController implements Initializable {
-//    private ObservableList<LoanSlip> loanSlips;
-//
-//    public LoanSlip getLoanSlip(String text) {
-//        for (LoanSlip loanSlip : loanSlips) {
-//            if (((loanSlip.getId()).equals((text)) || (loanSlip.getBillUser().equals(text)))) {
-//                return loanSlip;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    private Stage stage;
-//    private Scene scene;
-//    private Parent root;
-//
-//    public void goToLoginScene(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
-//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        scene = new Scene(root, 1080, 720);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    public void goToAddBookScene(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("AddBook.fxml"));
-//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        scene = new Scene(root, 1080, 720);
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-
-//    public void goToBookScene(ActionEvent event) throws IOException {
-//        Parent root = FXMLLoader.load(getClass().getResource("ListView.fxml"));
-//        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-//        scene = new Scene(root, 1080, 720);
-//        stage.setTitle("Home");
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//@FXML
-//TextField id;
-//@FXML
-//TextField BillUser;
-//    @FXML
-//    TextField Books;
-//    @FXML
-//    TextField date;
-//    @FXML
-//    TextField returnDate;
-//    @FXML
-//    TextField status;
-
-//    public void edit() {
-//        Book book = getBook(id.getText());
-//        if (book != null) {
-//            book.setId(id.getText());
-//            book.setTitle(title.getText());
-//            book.setAuthor(author.getText());
-//            book.setReleaseYear(releaseYear.getText());
-//            book.setGenre(genre.getText());
-//            book.setStatus(status.getText());
-//        }
-//    }
-
-//    @FXML
-//    TextField idAdd;
-//    @FXML
-//    TextField titleAdd;
-//    @FXML
-//    TextField authorAdd;
-//    @FXML
-//    TextField releaseYearAdd;
-//    @FXML
-//    TextField genreAdd;
-//    @FXML
-//    TextField statusAdd;
-//
-//    public boolean add() {
-//        Book book = getBook(idAdd.getText());
-//        Alert alert = new Alert(Alert.AlertType.INFORMATION);
-//        if (idAdd.getText().isEmpty() || titleAdd.getText().isEmpty() || authorAdd.getText().isEmpty() || releaseYearAdd.getText().isEmpty() || genreAdd.getText().isEmpty() || statusAdd.getText().isEmpty()) {
-//            alert.setContentText("Không được để trống");
-//            alert.show();
-//            return false;
-//        }
-//
-//        if (!statusAdd.getText().equalsIgnoreCase("true") && !statusAdd.getText().equalsIgnoreCase("false")) {
-//            alert.setContentText("Status phải là true hoặc false");
-//            alert.show();
-//            return false;
-//        }
-//
-//        try {
-//            Double num = Double.parseDouble(releaseYearAdd.getText());
-//        } catch (NumberFormatException e) {
-//            alert.setContentText("Hãy nhập đúng giá trị năm xuất bản");
-//            alert.show();
-//            return false;
-//        }
-//
-//        if (book == null) {
-//            Books.add(new Book(idAdd.getText(), titleAdd.getText(), authorAdd.getText(), releaseYearAdd.getText(), genreAdd.getText(), statusAdd.getText()));
-//            return true;
-//        } else {
-//            alert.setContentText("This id is exist. Try again please.");
-//            alert.show();
-//        }
-//        return false;
-//    }
-
-//    @FXML
-//    private TableView<Book> table;
-//    @FXML
-//    private TableColumn<Book, String> idCol;
-//    @FXML
-//    private TableColumn<Book, String> titleCol;
-//    @FXML
-//    private TableColumn<Book, String> authorCol;
-//    @FXML
-//    private TableColumn<Book, String> releaseYearCol;
-//    @FXML
-//    private TableColumn<Book, String> genreCol;
-//    @FXML
-//    private TableColumn<Book, String> statusCol;
-
-//    @Override
-//    public void initialize(URL url, ResourceBundle resourceBundle) {
-//        Books = FXCollections.observableArrayList(
-//                new Book("b1", "toan", "nhan", "2018", "Subject", "true"),
-//                new Book("b2", "tieng anh", "khanh", "2018", "Subject", "true"),
-//                new Book("b3", "one piece", "oda", "1999", "Animation", "true"),
-//                new Book("b4", "doraemon", "fuji", "2000", "Animation", "true")
-//        );
-//        idCol.setCellValueFactory(new PropertyValueFactory<Book, String>("Id"));
-//        titleCol.setCellValueFactory(new PropertyValueFactory<Book, String>("Title"));
-//        authorCol.setCellValueFactory(new PropertyValueFactory<Book, String>("Author"));
-//        releaseYearCol.setCellValueFactory(new PropertyValueFactory<Book, String>("ReleaseYear"));
-//        genreCol.setCellValueFactory(new PropertyValueFactory<Book, String>("Genre"));
-//        statusCol.setCellValueFactory(new PropertyValueFactory<Book, String>("Status"));
-//        table.setItems(Books);
-//    }
-//}
